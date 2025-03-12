d1d4b6c2add6a5dd4bf2e8c4f2407ad0559e9504
Daniela <dggtn97@gmail.com>
2025-03-12 19:21:28 -0300

    agrego a la clase Direccion @Entity y @id y @GeneratedValue + atributos como calle,provincia,codigo postal

--- src/main/java/com/ecommerce/ecommerce_backend/model/Cupon.java
+++ src/main/java/com/ecommerce/ecommerce_backend/model/Cupon.java
@@ -1,4 +1,9 @@
 package com.ecommerce.ecommerce_backend.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+@Id
 public class Cupon {
 }
--- src/main/java/com/ecommerce/ecommerce_backend/model/Direccion.java
+++ src/main/java/com/ecommerce/ecommerce_backend/model/Direccion.java
@@ -1,4 +1,29 @@
 package com.ecommerce.ecommerce_backend.model;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Direccion {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String nombre;
+
+    private String localidad;
+    private String direccion;
+
+    private String ciudad;
+
+    private String provincia;
+
+    private String codigoPostal;
+
+    private String telefono;
+
 }
