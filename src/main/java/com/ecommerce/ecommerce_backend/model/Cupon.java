0787806ac61e3867501ee03b773a34de2ae93687
Daniela <dggtn97@gmail.com>
2025-03-12 20:15:18 -0300

    agrego fecha inicio y fin para validacion de cupon

--- src/main/java/com/ecommerce/ecommerce_backend/model/Cupon.java
+++ src/main/java/com/ecommerce/ecommerce_backend/model/Cupon.java
@@ -2,8 +2,11 @@ package com.ecommerce.ecommerce_backend.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.*;
+
+import java.time.LocalDate;
 
 @Entity
 @Getter
@@ -12,5 +15,17 @@ import lombok.*;
 @NoArgsConstructor
 @EqualsAndHashCode
 public class Cupon {
+    @Id@GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String code;
+
+    private double discountPercentage;
+
+    private LocalDate validoInicioFecha;
+
+    private LocalDate validoFinFecha;
+
+
 
 }
