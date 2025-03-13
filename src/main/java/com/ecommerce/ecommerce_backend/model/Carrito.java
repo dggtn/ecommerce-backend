79423be3ad5581473a07d7564c41c15f21f17f76
Daniela <dggtn97@gmail.com>
2025-03-13 17:49:00 -0300

    creo la clase carrito de compras

---
+++ src/main/java/com/ecommerce/ecommerce_backend/model/Carrito.java
@@ -0,0 +1,27 @@
+package com.ecommerce.ecommerce_backend.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+public class Carrito {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @OneToOne
+    private Usuario usuario;
+
+    private Set<ItemsDelCarrito> itemsDelCarrito = new HashSet<>();
+
+
+
+}
