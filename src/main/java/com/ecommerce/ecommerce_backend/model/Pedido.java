0a631cea764a533ae3515827c34913e99f750db0
Daniela <dggtn97@gmail.com>
2025-03-13 19:25:44 -0300

    agrego persistencia y lombok

--- src/main/java/com/ecommerce/ecommerce_backend/model/Pedido.java
+++ src/main/java/com/ecommerce/ecommerce_backend/model/Pedido.java
@@ -1,4 +1,21 @@
 package com.ecommerce.ecommerce_backend.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+
+
 public class Pedido {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 }
