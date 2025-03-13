4f4316396c13fcb2a13bf7b653f675e1008e7e0b
Daniela <dggtn97@gmail.com>
2025-03-13 19:21:43 -0300

    agrego a clase detalleNegocio sus atributos como nombre, mail ,telefono,logo,direccion,banner

--- src/main/java/com/ecommerce/ecommerce_backend/model/DetalleNegocio.java
+++ src/main/java/com/ecommerce/ecommerce_backend/model/DetalleNegocio.java
@@ -1,4 +1,20 @@
 package com.ecommerce.ecommerce_backend.model;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class DetalleNegocio {
+    private String nombreNegocio;
+    private String mailNegocio;
+    private String telefonoNegocio;
+    private String direccionNegocio;
+    private String logo;
+    private String banner;
+
 }
