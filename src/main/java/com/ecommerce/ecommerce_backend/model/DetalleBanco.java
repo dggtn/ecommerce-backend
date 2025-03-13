02f113b83a7504f66c72ebfddbdba14fa2dd0881
Daniela <dggtn97@gmail.com>
2025-03-13 19:19:38 -0300

    agrego atributos a la cuenta detalleBanco

--- src/main/java/com/ecommerce/ecommerce_backend/model/DetalleBanco.java
+++ src/main/java/com/ecommerce/ecommerce_backend/model/DetalleBanco.java
@@ -1,4 +1,18 @@
 package com.ecommerce.ecommerce_backend.model;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class DetalleBanco {
+
+    private String numeroCuenta;
+    private String duenoCuenta;
+
+    private String codigoCVC;
+
 }
