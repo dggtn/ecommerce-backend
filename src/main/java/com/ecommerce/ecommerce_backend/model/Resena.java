fdc423994fb5fa7a33d475095d3e66debe8badb1
Daniela <dggtn97@gmail.com>
2025-03-13 18:38:26 -0300

    creo clase resena, agrego atributos y campos para BD

--- src/main/java/com/ecommerce/ecommerce_backend/model/Resena.java
+++ src/main/java/com/ecommerce/ecommerce_backend/model/Resena.java
@@ -1,4 +1,50 @@
 package com.ecommerce.ecommerce_backend.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
 public class Resena {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private String texto;
+
+    @Column(nullable = false)
+    private String rating;
+
+    @ElementCollection
+    private List<String> imagenesDelProducto;
+
+
+    @JsonIgnore
+    @ManyToOne
+    @JoinColumn(name="id_producto",nullable = false)
+    private Producto producto ;
+
+
+    @ManyToOne
+    @JoinColumn(name="user_id",nullable = false)
+    private Usuario usuario;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+
+
+
+
+
+
 }
