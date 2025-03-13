96c83f8fdd063b4cb2bc00bb9953a508acb3b9d6
Daniela <dggtn97@gmail.com>
2025-03-13 19:30:42 -0300

    importo persistencia entity y lombok

--- src/main/java/com/ecommerce/ecommerce_backend/model/PedidoItem.java
+++ src/main/java/com/ecommerce/ecommerce_backend/model/PedidoItem.java
@@ -1,4 +1,14 @@
 package com.ecommerce.ecommerce_backend.model;
 
+
+import jakarta.persistence.Entity;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
 public class PedidoItem {
 }
