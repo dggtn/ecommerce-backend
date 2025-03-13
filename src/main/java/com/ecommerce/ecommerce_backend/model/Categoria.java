197cebe590c447091c57cb6af6c3b3822ffe7ed5
Daniela <dggtn97@gmail.com>
2025-03-13 18:28:20 -0300

    agrego atributos a la clase Categoria

--- src/main/java/com/ecommerce/ecommerce_backend/model/Categoria.java
+++ src/main/java/com/ecommerce/ecommerce_backend/model/Categoria.java
@@ -1,4 +1,29 @@
 package com.ecommerce.ecommerce_backend.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+import org.antlr.v4.runtime.misc.NotNull;
+import org.hibernate.annotations.NotFound;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+
+
 public class Categoria {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String nombre;
+    @NotNull
+    @Column(unique = true)
+    private String categoriaId;
+    @ManyToOne
+    private Categoria categoriaPadre;
+    @NotNull
+    private Integer nivel;
+
 }
