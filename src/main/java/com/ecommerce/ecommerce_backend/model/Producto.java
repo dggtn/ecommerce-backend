3feb653c17f9534427aa18f6d0af7566cafaa850
Daniela <dggtn97@gmail.com>
2025-03-13 18:14:12 -0300

    creo la clase Vendedor

--- src/main/java/com/ecommerce/ecommerce_backend/model/Producto.java
+++ src/main/java/com/ecommerce/ecommerce_backend/model/Producto.java
@@ -1,10 +1,12 @@
 package com.ecommerce.ecommerce_backend.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -16,4 +18,35 @@ public class Producto {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    private String titulo;
+    private String descripcion;
+    private int preciomasIva;
+    private int precioDeVenta;
+    private int cantidad;
+    private String color;
+
+    @ElementCollection
+    private List<String> imagenes =  new ArrayList<>();
+
+    private int numRatings;
+
+    @ManyToOne
+    private Categoria categoria;
+
+    @ManyToOne
+    private Vendedor vendedor;
+
+    private LocalDateTime CreadoEn;
+
+    private String tamanio;
+
+    @OneToMany(mappedBy = "producto",cascade = CascadeType.ALL,orphanRemoval = true)
+    private List<Resena> resenas = new ArrayList<>();
+
+
+
+
+
+
 }
---
+++ src/main/java/com/ecommerce/ecommerce_backend/model/Vendedor.java
@@ -0,0 +1,4 @@
+package com.ecommerce.ecommerce_backend.model;
+
+public class Vendedor {
+}
