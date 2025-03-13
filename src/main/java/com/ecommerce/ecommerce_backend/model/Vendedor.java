8ec7973153c3a0e0205db7e838c32491e6016050
Daniela <dggtn97@gmail.com>
2025-03-13 19:08:55 -0300

    creo clase DetalleNegocio

--- src/main/java/com/ecommerce/ecommerce_backend/model/Vendedor.java
+++ src/main/java/com/ecommerce/ecommerce_backend/model/Vendedor.java
@@ -1,4 +1,50 @@
 package com.ecommerce.ecommerce_backend.model;
 
+
+import com.ecommerce.ecommerce_backend.dominio.EstatusCuenta;
+import com.ecommerce.ecommerce_backend.dominio.Roles;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
 public class Vendedor {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long id;
+
+    private String nombreVendedor;
+
+    private String telefono;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+    private String password;
+
+    @Embedded
+    private DetalleNegocio detallesNegocio = new DetalleNegocio();
+
+    @Embedded
+    private DetalleBanco detalleBanco = new DetalleBanco();
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private Direccion direccionEntrega = new Direccion();
+
+    private String GSTIN;
+    private Roles rol = Roles.ROL_USUARIO;
+    private boolean elEmailEsVerificado = false;
+
+    private EstatusCuenta estatusCuenta = EstatusCuenta.VERIFICACION_PENDIENTE;
+
+
+
+
+
+
+
 }
