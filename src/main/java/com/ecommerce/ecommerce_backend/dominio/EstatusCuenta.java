946c59243906ee4d9f23af98f385344a9ffaf2ef
Daniela <dggtn97@gmail.com>
2025-03-13 19:07:57 -0300

    creo clase EstatusCuenta

---
+++ src/main/java/com/ecommerce/ecommerce_backend/dominio/EstatusCuenta.java
@@ -0,0 +1,12 @@
+package com.ecommerce.ecommerce_backend.dominio;
+
+public enum EstatusCuenta {
+
+    VERIFICACION_PENDIENTE,
+    ACTIVO,
+    SUSPENDIDO,
+    DESACTIVADO,
+    BANNEADO,
+    CERRADO
+
+}
